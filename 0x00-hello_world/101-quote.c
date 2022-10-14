2ca8d863dc738c0840fd6608d7ec5003708ae71d
Petdrone <okodughaodion@gmail.com>
2022-10-13 21:57:00 -0700

    task8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <unistd.h>
 /**
- * main-program entry point.
- *
- * Return:1 on succcess.
- */
+*main-program entry point.
+*
+*Return:1 on succcess.
+*/
 int main(void)
 {
-	write(2,"and add that piece of aryt is useful\" - Dora Korpar,2015-10-19\n", 59);
-	return (1);
+write(2,"and add that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
 }
