97009c3d7d586d0e6e7d70d527165dd13829b0df
Petdrone <okodughaodion@gmail.com>
2022-10-13 21:54:01 -0700

    task8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+/**
+ * main-program entry point.
+ *
+ * Return:1 on succcess.
+ */
+int main(void)
+{
+	write(2,"and add that piece of aryt is useful\" - Dora Korpar,2015-10-19\n", 59);
+	return (1);
+}
