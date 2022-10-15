1a8867946898eb82bf43d46cb6363e8abfc1db7f
Petdrone <okodughaodion@gmail.com>
2022-10-15 06:54:50 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- *main- Serves as the entry point for the program
+ *main- Servies as the entry point for the program
  *
  *Return: By default returns zero if no erros otherwise returns non-zero value.
  */
-int main(void)
+int main()
 {
 puts("\"Programming is like building a muiltilingual puzzle");
 return (0);
